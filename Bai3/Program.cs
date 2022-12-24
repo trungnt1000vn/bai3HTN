fc66c5551d49e9b7bc275db33f25b97d80e70e1e
Trung Nguyễn <trungnt1000vn@gmail.com>
2022-12-24 17:58:26 +0700

    initialize list

---
+++ Bai3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> listStudent = new List<Student>();
+            List<Worker> workerList = new List<Worker>();
+            for(int i = 0; i < 10; i++)
+            {
+                listStudent.Add(new Student(i+1));
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                workerList.Add(new Worker());
+            }
+        }
+    }
+    abstract class Human
+    {
+        private string firstName;
+        private string lastName;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+    }
+    class Student : Human
+    {
+        private int grade;
+
+        public Student()
+        {
+        }
+        public Student (int grade)
+        {
+            this.grade = grade;
+        }
+
+        public int Grade { get => grade; set => grade = value; }
+    }
+    class Worker : Human
+    {
+        private float weekSalary;
+        private float workHoursPerDay;
+
+        public Worker()
+        {
+        }
+
+        public float WeekSalary { get => weekSalary; set => weekSalary = value; }
+        public float WorkHoursPerDay { get => workHoursPerDay; set => workHoursPerDay = value; }
+        public int MoneyPerHour()
+        {
+            int moneyperhours = 0;
+            return moneyperhours;
+        }
+
+    }
+}
